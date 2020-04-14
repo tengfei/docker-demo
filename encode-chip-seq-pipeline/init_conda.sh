759a47057f31bec85f1f46720aeadc54cbe98879
Tengfei Yin <tengfeiyin@Tengfeis-MacBook-Pro.local>
2020-04-14 17:58:29 -0400

    update docker file and script

--- encode-chip-seq-pipeline/init_conda.sh
+++ encode-chip-seq-pipeline/init_conda.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+# activating might now have effect outside script, so run it mannually to activate environment
+echo "porting ..."
 eval "$(conda shell.bash hook)"
+
+echo "activating encode-chip-seq-pipeline ... "
 conda activate encode-chip-seq-pipeline
+
+## put some scripts here 
+echo "initializing caper ..."
 caper init local
