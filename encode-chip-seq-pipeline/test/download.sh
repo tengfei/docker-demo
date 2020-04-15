830ec2158efb07bfbaef7e5849890eebe83c7afe
Tengfei Yin <tengfeiyin@Tengfeis-MacBook-Pro.local>
2020-04-15 14:19:18 -0400

    fix typo

--- encode-chip-seq-pipeline/test/download.sh
+++ encode-chip-seq-pipeline/test/download.sh
@@ -2,4 +2,4 @@
 
 wget https://raw.githubusercontent.com/tengfei/docker-demo/master/encode-chip-seq-pipeline/test/bip_ENCSR000DYI_subsampled.json
 wget https://raw.githubusercontent.com/tengfei/docker-demo/master/encode-chip-seq-pipeline/test/hg38_bip.tsv
-nohup caper run /chip-seq-pipeline2/chip.wdl -i bip_ENCSR000DYI_subsamped.json  &
+nohup caper run /chip-seq-pipeline2/chip.wdl -i bip_ENCSR000DYI_subsampled.json  &
