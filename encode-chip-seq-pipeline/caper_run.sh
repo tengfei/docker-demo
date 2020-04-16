16b241ea9104d10ff5b11e707bdc643b517a9973
Tengfei Yin <tengfeiyin@Tengfeis-MacBook-Pro.local>
2020-04-16 12:32:43 -0400

    get download script

--- encode-chip-seq-pipeline/caper_run.sh
+++ encode-chip-seq-pipeline/caper_run.sh
@@ -9,3 +9,8 @@ conda activate encode-chip-seq-pipeline
 echo "running caper ..."
 INPUT_JSON=$1
 caper run /chip-seq-pipeline2/chip.wdl -i $INPUT_JSON
+
+## organize output
+## croo metadata.json --method copy --out-def-json --out-dir /enigma/local_storage/chipseq-run/
+## organize qc files
+## qc2tsv /sample1/qc.json gs://sample2/qc.json s3://sample3/qc.json ... > spreadsheet.tsv
---
+++ encode-chip-seq-pipeline/download_test_job.sh
@@ -0,0 +1,5 @@
+RUN wget https://raw.githubusercontent.com/tengfei/docker-demo/master/encode-chip-seq-pipeline/test/bip_ENCSR000DYI_subsampled.json
+RUN wget https://raw.githubusercontent.com/tengfei/docker-demo/master/encode-chip-seq-pipeline/test/hg38_bip.tsv
+RUN wget https://raw.githubusercontent.com/tengfei/docker-demo/master/encode-chip-seq-pipeline/test/bip_ENCSR000DYI.json
+RUN wget https://raw.githubusercontent.com/tengfei/docker-demo/master/encode-chip-seq-pipeline/test/bip_ENCSR203KEU.json
+
