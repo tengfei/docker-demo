a5bfda390fdb1a6f04e21f8d5baecaac47cce2d0
Tengfei Yin <tengfeiyin@Tengfeis-MacBook-Pro.local>
2020-04-16 18:24:40 -0400

    update script

--- encode-chip-seq-pipeline/caper_run.sh
+++ encode-chip-seq-pipeline/caper_run.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-# run those script in your terminal (NOT as shell script) to activate env
+## Step1: activating conda env
+## Run those script in your terminal (NOT as shell script) to activate env
 echo "porting ..."
 eval "$(conda shell.bash hook)"
-
 echo "activating encode-chip-seq-pipeline ... "
 conda activate encode-chip-seq-pipeline
 
+## Step2: caper run the wdl file with input json
 echo "running caper ..."
 INPUT_JSON=$1
 caper run /chip-seq-pipeline2/chip.wdl -i $INPUT_JSON
 
-## Organize output
+## Step3: croo to organize output
 ## WARNING: by default if multiple task found, using latest one, manually specify one to collect if needed
-## this is used for automation assuming only one task executed. 
-METAFILE=$(ls -t ~/chipseq-run/chip/*/metadata.json| head -1)
-croo $METAFILE --method copy  --out-dir /enigma/local_storage/chipseq-run/
-## organize qc files into single tsv file
-## hard coded for demo purse
-qc2tsv /enigma/local_storage/chipseq-run/qc/qc.json > /enigma/local_storage/chipseq-run/qc-all.tsv
+## This is used for automation assuming only one task executed. 
+## Assume it's in the folder where this script is executed e.g. in default ~/chipseq-run/
+echo "croo is collecting outputs"
+METAFILE=$(ls -t ./chip/*/metadata.json| head -1)
+OUTDIR=${2:-/enigma/local_storage/chipseq-run}
+OUTDIR=$(realpath $OUTDIR)
+croo $METAFILE --method copy  --out-dir $OUTDIR 
+
+## Step4: qc2tsv to organize qc files into single tsv file
+qc2tsv $OUTDIR/qc/qc.json > $OUTDIR/qc/qc-all.tsv
